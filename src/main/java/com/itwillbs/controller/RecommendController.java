b6b940d60bdfd493b37d1b9b4c18cae7038921fb
ziwoneee <ziwonee24@gmail.com>
2025-06-12 12:48:39 +0900

    RecommendController 및 recommend_page_01.jsp 생성 - 구매기반추천

--- src/main/java/com/itwillbs/controller/RecommendController.java
+++ src/main/java/com/itwillbs/controller/RecommendController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value = "/recommend/*")
@@ -11,5 +12,12 @@ public class RecommendController {
 	
 	//mylog
 	private static final Logger logger = LoggerFactory.getLogger(RecommendController.class);
+	
+	// 구매 기반 추천 (GET)
+	@RequestMapping(value = "/byPurchase", method = RequestMethod.GET)
+	public String recommendByPurchase() {
+		logger.info(" recommendByPurchase() 호출 ");
+		return "recommend/recommend_page_01";
+	}
 
 } // RecommendController 끝
---
+++ src/main/webapp/WEB-INF/views/recommend/recommend_page_01.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>구매 기반 추천</title>
+</head>
+<body>
+	<h1> 구매 기반 추천 도서 </h1>
+
+</body>
+</html>
