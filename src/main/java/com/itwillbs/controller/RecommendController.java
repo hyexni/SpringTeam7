0b5ea8b631e73e7dd02a500be370a8fb7c0e297f
ziwoneee <ziwonee24@gmail.com>
2025-06-12 12:58:43 +0900

    RecommendController 생성완료

--- src/main/java/com/itwillbs/controller/RecommendController.java
+++ src/main/java/com/itwillbs/controller/RecommendController.java
@@ -13,11 +13,38 @@ public class RecommendController {
 	//mylog
 	private static final Logger logger = LoggerFactory.getLogger(RecommendController.class);
 	
+	
 	// 구매 기반 추천 (GET)
 	@RequestMapping(value = "/byPurchase", method = RequestMethod.GET)
 	public String recommendByPurchase() {
 		logger.info(" recommendByPurchase() 호출 ");
 		return "recommend/recommend_page_01";
 	}
+	
+	
+	// 찜한 도서 추천 (GET)
+	@RequestMapping(value = "/byWishlist", method = RequestMethod.GET)
+	public String recommendByWishlist() {
+		logger.info(" recommendByWishlist() 호출 ");
+		return "recommend/recommend_page_02";
+	}
+	
+	
+	// 추천 정렬 (GET)
+	@RequestMapping(value = "/sort?type=", method = RequestMethod.GET)
+	public String sortRecommend() {
+		logger.info(" sortRecommend() 호출 ");
+		return "recommend/recommend_page_03";
+	}
+	
+	
+	// 추천 결과 출력 (GET)
+	@RequestMapping(value = "/result", method = RequestMethod.GET)
+	public String recommendResult() {
+		logger.info(" recommendResult() 호출 ");
+		return "recommend/recommend_page_04";
+	}
+	
 
 } // RecommendController 끝
+
